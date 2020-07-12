decde902926470d24f9a826225c5d59776685a80
蒋颖 <jiangying@jiangyingdeMacBook-Pro-3.local>
2020-07-12 23:20:24 +0800

    新增log4j和slf4j引入和使用

--- src/main/java/com/testng/BasicAnnotation.java
+++ src/main/java/com/testng/BasicAnnotation.java
@@ -1,11 +1,13 @@
 package com.testng;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class BasicAnnotation {
-
+    final Logger logger = LoggerFactory.getLogger(BasicAnnotation.class);
     @Test
     public void testCase1(){
-
+        logger.info("开始");
     }
 }
